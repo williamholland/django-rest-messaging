cf1c1d96095b0c9fd51be69246510665940c4055
Will Holland <snowy.the.crow@gmail.com>
2020-04-21 00:15:05 +0700

    Fix middleware

--- rest_messaging/middleware.py
+++ rest_messaging/middleware.py
@@ -5,9 +5,10 @@
 from __future__ import unicode_literals
 from django.core.cache import cache
 from rest_messaging.models import Participant
+from django.utils.deprecation import MiddlewareMixin
 
 
-class MessagingMiddleware(object):
+class MessagingMiddleware(MiddlewareMixin):
     """
     Ensures we can access request.user as request.rest_messaging_participant in every request.
     """
